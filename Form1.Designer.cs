94102d56cd0b3fefbf361aef75a4a95d45cf7202
gramz <gramz@LAB4-CAT4>
2024-02-01 09:19:07 -0600

    agregue imagen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,81 +44,92 @@ namespace ControlPersonas
             this.labelFecha = new System.Windows.Forms.Label();
             this.buttonMostrar = new System.Windows.Forms.Button();
             this.buttonPrimeraMayuscula = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxDpi
             // 
-            this.textBoxDpi.Location = new System.Drawing.Point(311, 65);
+            this.textBoxDpi.Location = new System.Drawing.Point(207, 42);
+            this.textBoxDpi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxDpi.Name = "textBoxDpi";
-            this.textBoxDpi.Size = new System.Drawing.Size(158, 26);
+            this.textBoxDpi.Size = new System.Drawing.Size(107, 20);
             this.textBoxDpi.TabIndex = 0;
             this.textBoxDpi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBoxNombre
             // 
-            this.textBoxNombre.Location = new System.Drawing.Point(311, 121);
+            this.textBoxNombre.Location = new System.Drawing.Point(207, 79);
+            this.textBoxNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxNombre.Name = "textBoxNombre";
-            this.textBoxNombre.Size = new System.Drawing.Size(158, 26);
+            this.textBoxNombre.Size = new System.Drawing.Size(107, 20);
             this.textBoxNombre.TabIndex = 1;
             this.textBoxNombre.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBoxApellido
             // 
-            this.textBoxApellido.Location = new System.Drawing.Point(311, 190);
+            this.textBoxApellido.Location = new System.Drawing.Point(207, 123);
+            this.textBoxApellido.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxApellido.Name = "textBoxApellido";
-            this.textBoxApellido.Size = new System.Drawing.Size(158, 26);
+            this.textBoxApellido.Size = new System.Drawing.Size(107, 20);
             this.textBoxApellido.TabIndex = 2;
             this.textBoxApellido.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 68);
+            this.label1.Location = new System.Drawing.Point(47, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.Size = new System.Drawing.Size(25, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "DPI";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 124);
+            this.label2.Location = new System.Drawing.Point(47, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Nombre";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(71, 193);
+            this.label3.Location = new System.Drawing.Point(47, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Apellido";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(71, 286);
+            this.label4.Location = new System.Drawing.Point(47, 186);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(108, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Fecha de Nacimiento";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(311, 276);
+            this.dateTimePicker1.Location = new System.Drawing.Point(207, 179);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 20);
             this.dateTimePicker1.TabIndex = 7;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // buttonIngreso
             // 
-            this.buttonIngreso.Location = new System.Drawing.Point(311, 368);
+            this.buttonIngreso.Location = new System.Drawing.Point(207, 239);
+            this.buttonIngreso.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonIngreso.Name = "buttonIngreso";
-            this.buttonIngreso.Size = new System.Drawing.Size(175, 64);
+            this.buttonIngreso.Size = new System.Drawing.Size(117, 42);
             this.buttonIngreso.TabIndex = 8;
             this.buttonIngreso.Text = "Ingresar Persona";
             this.buttonIngreso.UseVisualStyleBackColor = true;
@@ -127,9 +138,10 @@ namespace ControlPersonas
             // labelDpi
             // 
             this.labelDpi.AutoSize = true;
-            this.labelDpi.Location = new System.Drawing.Point(755, 68);
+            this.labelDpi.Location = new System.Drawing.Point(503, 44);
+            this.labelDpi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDpi.Name = "labelDpi";
-            this.labelDpi.Size = new System.Drawing.Size(51, 20);
+            this.labelDpi.Size = new System.Drawing.Size(35, 13);
             this.labelDpi.TabIndex = 9;
             this.labelDpi.Text = "label5";
             this.labelDpi.Visible = false;
@@ -137,9 +149,10 @@ namespace ControlPersonas
             // labelNombre
             // 
             this.labelNombre.AutoSize = true;
-            this.labelNombre.Location = new System.Drawing.Point(759, 124);
+            this.labelNombre.Location = new System.Drawing.Point(506, 81);
+            this.labelNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNombre.Name = "labelNombre";
-            this.labelNombre.Size = new System.Drawing.Size(51, 20);
+            this.labelNombre.Size = new System.Drawing.Size(35, 13);
             this.labelNombre.TabIndex = 10;
             this.labelNombre.Text = "label6";
             this.labelNombre.Visible = false;
@@ -147,9 +160,10 @@ namespace ControlPersonas
             // labelApellido
             // 
             this.labelApellido.AutoSize = true;
-            this.labelApellido.Location = new System.Drawing.Point(759, 193);
+            this.labelApellido.Location = new System.Drawing.Point(506, 125);
+            this.labelApellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelApellido.Name = "labelApellido";
-            this.labelApellido.Size = new System.Drawing.Size(51, 20);
+            this.labelApellido.Size = new System.Drawing.Size(35, 13);
             this.labelApellido.TabIndex = 11;
             this.labelApellido.Text = "label7";
             this.labelApellido.Visible = false;
@@ -157,18 +171,20 @@ namespace ControlPersonas
             // labelFecha
             // 
             this.labelFecha.AutoSize = true;
-            this.labelFecha.Location = new System.Drawing.Point(759, 276);
+            this.labelFecha.Location = new System.Drawing.Point(506, 179);
+            this.labelFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelFecha.Name = "labelFecha";
-            this.labelFecha.Size = new System.Drawing.Size(51, 20);
+            this.labelFecha.Size = new System.Drawing.Size(35, 13);
             this.labelFecha.TabIndex = 12;
             this.labelFecha.Text = "label8";
             this.labelFecha.Visible = false;
             // 
             // buttonMostrar
             // 
-            this.buttonMostrar.Location = new System.Drawing.Point(595, 368);
+            this.buttonMostrar.Location = new System.Drawing.Point(397, 239);
+            this.buttonMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonMostrar.Name = "buttonMostrar";
-            this.buttonMostrar.Size = new System.Drawing.Size(156, 64);
+            this.buttonMostrar.Size = new System.Drawing.Size(104, 42);
             this.buttonMostrar.TabIndex = 13;
             this.buttonMostrar.Text = "Mostrar Persona";
             this.buttonMostrar.UseVisualStyleBackColor = true;
@@ -176,19 +192,30 @@ namespace ControlPersonas
             // 
             // buttonPrimeraMayuscula
             // 
-            this.buttonPrimeraMayuscula.Location = new System.Drawing.Point(803, 368);
+            this.buttonPrimeraMayuscula.Location = new System.Drawing.Point(535, 239);
+            this.buttonPrimeraMayuscula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPrimeraMayuscula.Name = "buttonPrimeraMayuscula";
-            this.buttonPrimeraMayuscula.Size = new System.Drawing.Size(158, 64);
+            this.buttonPrimeraMayuscula.Size = new System.Drawing.Size(105, 42);
             this.buttonPrimeraMayuscula.TabIndex = 14;
             this.buttonPrimeraMayuscula.Text = "Primera Mayúscula";
             this.buttonPrimeraMayuscula.UseVisualStyleBackColor = true;
             this.buttonPrimeraMayuscula.Click += new System.EventHandler(this.buttonPrimeraMayuscula_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ControlPersonas.Properties.Resources.Servidor_LAMP___HTTPS_con_https;
+            this.pictureBox1.Location = new System.Drawing.Point(581, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 111);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1065, 494);
+            this.ClientSize = new System.Drawing.Size(710, 321);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonPrimeraMayuscula);
             this.Controls.Add(this.buttonMostrar);
             this.Controls.Add(this.labelFecha);
@@ -204,8 +231,10 @@ namespace ControlPersonas
             this.Controls.Add(this.textBoxApellido);
             this.Controls.Add(this.textBoxNombre);
             this.Controls.Add(this.textBoxDpi);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +257,7 @@ namespace ControlPersonas
         private System.Windows.Forms.Label labelFecha;
         private System.Windows.Forms.Button buttonMostrar;
         private System.Windows.Forms.Button buttonPrimeraMayuscula;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
